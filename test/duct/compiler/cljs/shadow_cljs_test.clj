1e623397aeec14178351c4e70e43c2acc889b10c
James Reeves <jreeves@weavejester.com>
2024-12-07 00:00:24 +0000

    Initial commit

---
+++ test/duct/compiler/cljs/shadow_cljs_test.clj
@@ -0,0 +1,7 @@
+(ns duct.compiler.cljs.shadow-cljs-test
+  (:require [clojure.test :refer :all]
+            [duct.compiler.cljs.shadow-cljs :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
