83cba73abeaee9f7d73c6e84e8c4f475992625de
James Reeves <jreeves@weavejester.com>
2024-12-07 20:47:49 +0000

    Change Integrant method keys

--- src/duct/compiler/cljs/shadow.clj
+++ src/duct/compiler/cljs/shadow.clj
@@ -1,5 +1,11 @@
 (ns duct.compiler.cljs.shadow
   (:require [integrant.core :as ig]))
 
-(defmethod ig/init-key :duct.compiler.cljs/shadow [_ _options]
+(defmethod ig/init-key ::release [_ _options]
   (prn :compiled))
+
+(defmethod ig/init-key ::watch [_ _options]
+  (prn :worker-started))
+
+(defmethod ig/halt-key! ::watch [_ _worker]
+  (prn :worker-stopped))
