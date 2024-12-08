6d7309e4f95efaa2ab4b08511fc129fe9e81cf4e
James Reeves <jreeves@weavejester.com>
2024-12-08 23:10:14 +0000

    Implement ::release component

--- src/duct/compiler/cljs/shadow.clj
+++ src/duct/compiler/cljs/shadow.clj
@@ -1,8 +1,35 @@
 (ns duct.compiler.cljs.shadow
-  (:require [integrant.core :as ig]))
+  (:require [integrant.core :as ig]
+            [shadow.cljs.devtools.api :as api]
+            [shadow.cljs.devtools.config :as config]
+            [shadow.cljs.devtools.server.common :as common]
+            [shadow.cljs.devtools.server.runtime :as runtime]
+            [shadow.runtime.services :as rt]))
 
-(defmethod ig/init-key ::release [_ _options]
-  (prn :compiled))
+(defn- make-runtime [config]
+  (-> {::api/started (System/currentTimeMillis), :config config}
+      (rt/init (common/get-system-config config))
+      (rt/start-all)))
+
+(defmacro ^:private with-runtime [runtime & body]
+  `(let [runtime# ~runtime]
+     (try
+       (runtime/set-instance! runtime#)
+       (do ~@body)
+       (finally
+         (runtime/reset-instance!)
+         (rt/stop-all runtime#)))))
+
+(defn- normalize-config [config]
+  (-> (merge config/default-config config config)
+      (dissoc :build)
+      (assoc-in [:builds :cljs] (:build config))
+      (config/normalize)))
+
+(defmethod ig/init-key ::release [_ config]
+  (let [config (normalize-config config)]
+    (with-runtime (make-runtime config)
+      (api/release* (-> config :builds :cljs) {}))))
 
 (defmethod ig/init-key ::watch [_ _options]
   (prn :worker-started))
--- test/duct/compiler/cljs/shadow_test.clj
+++ test/duct/compiler/cljs/shadow_test.clj
@@ -1,7 +1,20 @@
 (ns duct.compiler.cljs.shadow-test
-  (:require [clojure.test :refer :all]
-            [duct.compiler.cljs.shadow :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [clojure.java.io :as io]
+            [duct.compiler.cljs.shadow :as shadow]
+            [integrant.core :as ig]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest release-test
+  (try
+    (ig/init-key
+     ::shadow/release
+     {:build
+      {:target :browser
+       :output-dir "target/cljs/js"
+       :asset-path "/js"
+       :modules {:main {:entries ['duct.compiler.cljs.client-test]}}}})
+    (is (.exists (io/file "target/cljs/js/main.js")))
+    (finally
+      (let [f (io/file "target/cljs/js/main.js")]
+        (when (.exists f)
+          (.delete f))))))
