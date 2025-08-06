d6c7c1a690fe941301d56f5c88b6f9520e744873
James Reeves <jreeves@weavejester.com>
2025-08-06 15:41:56 +0100

    Release 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.duct-framework/compiler.cljs.shadow "0.1.2"
+(defproject org.duct-framework/compiler.cljs.shadow "0.1.3"
   :description "Integrant methods for running Shadow CLJS"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
