c1631548bb416e47acb0efd94c106639d6aca0cb
James Reeves <jreeves@weavejester.com>
2025-08-06 15:46:02 +0100

    Add URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.duct-framework/compiler.cljs.shadow "0.1.3"
   :description "Integrant methods for running Shadow CLJS"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/duct-framework/compiler.cljs.shadow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.1"]
