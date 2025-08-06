24a6a8f176fa23079641499e0d3fda0f3da93e29
James Reeves <jreeves@weavejester.com>
2025-08-06 15:33:25 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.1"]
                  [integrant "0.13.1"]
-                 [thheller/shadow-cljs "2.28.20"]])
+                 [thheller/shadow-cljs "3.1.8"]])
